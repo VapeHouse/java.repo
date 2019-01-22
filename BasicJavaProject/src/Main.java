0d4c66a2bd6dd578ed6a59f0194ceb5e730cb32d
John Otugo <jtotugo@ymail.com>
2019-01-22 06:09:58 +0000

    Added a new number code

--- BasicJavaProject/src/Main.java
+++ BasicJavaProject/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Please enter your favourite number");
+
+        Scanner myInput = new Scanner(System.in);
+        String number = myInput.nextLine();
+
+        System.out.println(number);
     }
 }
